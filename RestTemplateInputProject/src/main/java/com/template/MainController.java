befacbce85944608d449638c246bb071286378f7
Piyush Garg <88052849+piyushgarg19@users.noreply.github.com>
2023-08-14 14:54:19 +0530

    Added RestTemplateInputProject Training Practice Project

---
+++ RestTemplateInputProject/src/main/java/com/template/MainController.java
@@ -0,0 +1,16 @@
+package com.template;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@GetMapping("/productinfo")
+	public Product getProduct() {
+		Product product=new Product();
+		product.setPid("10");
+		product.setProdname("Sugar");
+		return product;
+	}
+
+}
---
+++ RestTemplateInputProject/src/main/java/com/template/RestTemplateInputProjecApplication.java
@@ -0,0 +1,13 @@
+package com.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateInputProjecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateInputProjecApplication.class, args);
+	}
+
+}
