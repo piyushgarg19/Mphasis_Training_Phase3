8e2b7b5a0efbfe0f05f2a8b5b32b45f724e525c5
Piyush Garg <88052849+piyushgarg19@users.noreply.github.com>
2023-08-14 14:11:41 +0530

    Added DIExample Training Practice Code

---
+++ DIExample/src/main/java/com/DI/SpellChecker.java
@@ -0,0 +1,15 @@
+package com.DI;
+
+public class SpellChecker {
+	//Constructor
+		public SpellChecker() {
+			System.out.println("Spell Checker Constructor");
+		}
+		
+	//Method
+		
+		public void checkSpelling() {
+			System.out.println("checking is going on");
+		}
+
+}
